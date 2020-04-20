079c77e0aab70cbeb9c69f997dd394b5a4ec26ed
Chris <christophermyoder@gmail.com>
2020-04-20 14:37:05 -0500

    eod
    4/20/2020 eod

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Quiz from "./Quiz.svelte";
+  export let name;
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+  }
+</style>
+
+<main>
+	<h1>Hello {name}</h1>
+  
+  <Quiz quizName='New Quiz'/>
+  
+  
+</main>
---
+++ src/Quiz.svelte
@@ -0,0 +1,19 @@
+<div>
+	<h2>{quizName}</h2>
+	<!-- <h4>{title}</h4>
+	<input type="text" bind:value={title}> -->
+  <input type="number" bind:value={a}>
+  <input type="number" bind:value={b}>
+  <p>{a + b}</p>
+</div>
+
+
+<script>
+  export let quizName = "Chris Quiz";
+  // export let title = "";
+  let a = 0;
+  let b = 0;
+</script>
+
+<style>
+</style>
